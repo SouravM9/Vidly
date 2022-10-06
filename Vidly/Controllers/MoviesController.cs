8c7ab3a7bca9b8b5773f1b5b389054f1e5d3928f
SouravM9 <majumdars598@gmail.com>
2022-10-06 19:05:57 +0530

    initial

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //GET: Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Vikram Vedha!" };
+            var customers = new List<Customer>
+            {   new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //return View(movie);
+            return View(viewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("ID - " + id);
+        }
+
+        // Movies
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [HttpGet("Movies/ByReleaseDate/{year}/{month}")]  // New way to add custom route
+        public IActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + " " + month);
+        }
+    }
+}
---
+++ Vidly/help.cs
@@ -0,0 +1,35 @@
+﻿using System.Buffers.Text;
+using System.Reflection;
+using System;
+
+namespace Vidly
+{
+    public class help
+    {
+        //00:00 Introduction
+        //02:48 ASP.NET MVC Architecture
+        //05:31 Setting Up the Development Environment
+        //07:38 Your First ASP.NET MVC 5 App
+        //14:34 MVC in Action
+        //21:18 Adding a Theme
+        //26:49 Action Results
+        //31:47 Action Parameters
+        //37:42 Convention-based Routing
+        //42:58 Attribute-based Routing
+        //46:27 Passing Data to the Views
+        //50:30 View Models
+        //54:19 Razor Views
+        //58:38 Partial Views
+
+
+        // To apply your own Bootstrap
+        // 1. Go to https://bootswatch.com/
+        // 2. Download the .css file
+        // 3. Rename it to bootstrap-theme.css
+        // 4. Then add this CSS to _Layout.cshtml file
+        //     <link rel = "stylesheet" href="~/lib/bootstrap/dist/css/bootstrap-journal.css" />
+        // 5. go to Bootstrap (https://getbootstrap.com/docs/5.0/getting-started/introduction/) and copy the Bundle
+        // 6. Replace this line so that we are now using the latest version of bootstrap
+        //   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    }
+}
