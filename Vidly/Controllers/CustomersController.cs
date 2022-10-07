27f9f9f4ef475ad8ec0651fcf7c2008c2c699466
SouravM9 <majumdars598@gmail.com>
2022-10-07 15:21:29 +0530

    Added Movie & Customer List

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer(){ Id=1, Name="John Smith"},
+                new Customer(){ Id=2, Name="Mary Williams"}
+            };
+        }
+        public IActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customers = GetCustomers();
+
+            foreach (var customer in customers)
+            {
+                if (customer.Id == id)
+                    return View(customer);
+            }
+            return NotFound();
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -6,46 +6,63 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        //GET: Movies/Random
-        public IActionResult Random()
-        {
-            var movie = new Movie() { Name = "Vikram Vedha!" };
-            var customers = new List<Customer>
-            {   new Customer { Name = "Customer 1"},
-                new Customer { Name = "Customer 2"}
-            };
+        ////GET: Movies/Random
+        //public IActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Vikram Vedha!" };
+        //    var customers = new List<Customer>
+        //    {   new Customer { Name = "John Smith"},
+        //        new Customer { Name = "Mary Williams"}
+        //    };
 
-            var viewModel = new RandomMovieViewModel
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    //return View(movie);
+        //    return View(viewModel);
+        //}
+
+        //public IActionResult Edit(int id)
+        //{
+        //    return Content("ID - " + id);
+        //}
+
+        //// Movies
+        //public IActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
+
+        //[HttpGet("Movies/ByReleaseDate/{year}/{month}")]  // New way to add custom route
+        //public IActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + " " + month);
+        //}
+
+        public IActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
             {
-                Movie = movie,
-                Customers = customers
+                new Movie{ Id=1, Name="Vikram Vedha"},
+                new Movie{ Id=2, Name="RRR"}
             };
-
-            //return View(movie);
-            return View(viewModel);
         }
 
-        public IActionResult Edit(int id)
-        {
-            return Content("ID - " + id);
-        }
-
-        // Movies
-        public IActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        [HttpGet("Movies/ByReleaseDate/{year}/{month}")]  // New way to add custom route
-        public IActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + " " + month);
-        }
+        
     }
 }
